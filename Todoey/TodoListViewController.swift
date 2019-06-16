17e8e3ffa1782a2f8f5ef7d50a2f68534c59d311
NaderKaabi <naderaabi@gmail.com>
2019-06-16 02:17:50 +0100

    Data persisted using UserDefaults method

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -11,12 +11,17 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike","Buy Eggos","Destory Demogorgon"]
+    // interface to userdefaults database where storing key-value paires persistently across lunching our App
+    let defaults = UserDefaults.standard
 
    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if  let items = defaults.array(forKey: "TodoListArray") as? [String] {
+            itemArray = items
+        }
     }
 
     
@@ -53,16 +58,14 @@ class TodoListViewController: UITableViewController {
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         var textfield = UITextField()
-        let alret = UIAlertController(title: "AddNew Todoey Item", message: "", preferredStyle: .alert)
+        let alret = UIAlertController(title: "AddNew Todoey Item", message: "wrtie anything here", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user clicks the Add Item button on our UIAlret
             // print("Success!")
             //print(textfield.text!)
             self.itemArray.append(textfield.text!)
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
             self.tableView.reloadData()
-            
-            
-            
             
         }
             alret.addTextField {
